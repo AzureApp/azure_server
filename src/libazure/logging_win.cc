93ed5e02f9ac023d2eb81d8ee104b8c42ea1d6eb
Satori <comfycallum@gmail.com>
2019-07-18 14:24:44 +0100

    Print to console as well as debug for windows logs

--- src/libazure/logging_win.cc
+++ src/libazure/logging_win.cc
@@ -8,6 +8,9 @@ namespace azure {
 std::string DefaultLogLocation() { return "./azure.log"; }
 
 void WriteToLog(int level, const char* fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+
   if (!strstr(fmt, "\n")) {
     fmt = concat(fmt, "\n");
   }
@@ -18,7 +21,12 @@ void WriteToLog(int level, const char* fmt, ...) {
   strncpy_s(result, sizeof(result), logger, sizeof(logger));
   strcat_s(result, sizeof(result), fmt);
 
+  vsprintf(result, fmt, args);
+  va_end(args);
+
   OutputDebugStringA(result);
+
+  printf("%s\n", result);
 }
 
 }  // namespace azure
