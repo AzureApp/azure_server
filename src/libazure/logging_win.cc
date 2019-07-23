65cb1e8158700214d2ef1797dcb06308c2cc972d
Satori <comfycallum@gmail.com>
2019-07-23 13:30:22 +0100

    Fix erroneous logging on windows

--- src/libazure/logging_win.cc
+++ src/libazure/logging_win.cc
@@ -11,18 +11,18 @@ void WriteToLog(int level, const char* fmt, ...) {
   va_list args;
   va_start(args, fmt);
 
-  if (!strstr(fmt, "\n")) {
-    fmt = concat(fmt, "\n");
-  }
-
   const char logger[] = "[Azure Daemon] ";
-  char result[1024];
+  char result[1024] = {0};
 
   strncpy_s(result, sizeof(result), logger, sizeof(logger));
   strcat_s(result, sizeof(result), fmt);
 
   vsprintf(result, fmt, args);
   va_end(args);
+
+  if (!strstr(result, "\n")) {
+    result[strlen(result) + 1] = '\n';
+  }
 
   OutputDebugStringA(result);
 
